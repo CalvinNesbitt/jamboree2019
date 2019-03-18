3582aa2a6a2cfb9b508316ea01db1bfb8a2704ae
Oliver Street <o.street18@imperial.ac.uk>
2019-03-18 15:17:52 +0000

    noon_march data

--- weather_UoR/1200_data.py
+++ weather_UoR/1200_data.py
@@ -25,7 +25,7 @@ column_list = list(weather_no_nan.columns.values)[2:len(weather_no_nan.columns.v
 for i in column_list:
     weather_no_nan[i] = pd.to_numeric(weather_no_nan[i])
     
-    
+
 
 
 noon = weather_no_nan[weather_no_nan['Time']=='1200'].reset_index(drop=True).drop(labels=['Time'],axis=1)
@@ -33,3 +33,18 @@ noon = weather_no_nan[weather_no_nan['Time']=='1200'].reset_index(drop=True).dro
 print(noon.mean())
 print(noon.var())
 
+
+noon['Month'] = noon['TimeStamp'].map(lambda x: x.month)
+
+## restrict to March    
+weather_no_nan['Month'] = weather_no_nan['TimeStamp'].map(lambda x: x.month)
+weather_no_nan_march = weather_no_nan[weather_no_nan['Month'] == 3].reset_index(drop = True).drop(labels=['Month'],axis=1)
+
+noon_march = weather_no_nan_march[weather_no_nan_march['Time']=='1200'].reset_index(drop=True).drop(labels=['Time'],axis=1)
+
+print(noon_march.mean())
+print(noon_march.var())
+
+for i in column_list:
+
+    noon_march[i].to_csv(str(i) + '_noon_march.txt', index = False)

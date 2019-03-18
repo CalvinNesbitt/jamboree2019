58cda64fecf6b8c0a491cf814cac7b7c40fff7ff
Mariana Clare <mdac12@hotmail.co.uk>
2019-03-18 13:10:23 +0000

    updated csv

--- weather_UoR/read_in_data.py
+++ weather_UoR/read_in_data.py
@@ -8,10 +8,22 @@ Created on Mon Mar 18 10:56:07 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 weather = pd.read_csv('weather.csv')
 
-weather_no_nan = weather.dropna()
+weather_no_nan = weather.dropna().reset_index(drop= True)[1:len(weather)].reset_index(drop= True)
 
-plt.matshow(weather_no_nan.corr())
-plt.show()
+column_list = list(weather_no_nan.columns.values)
+
+
+for i in column_list:
+    weather_no_nan[i] = pd.to_numeric(weather_no_nan[i])
+
+print(weather_no_nan.info())
+corr = weather_no_nan.corr()
+
+sns.heatmap(corr, 
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)

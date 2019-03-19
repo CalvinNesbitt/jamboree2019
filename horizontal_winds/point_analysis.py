bac982e5d3700f58a0df0847aa6ac6aed4166bda
Thomas Gregory <tjg2118@ic.ac.uk>
2019-03-19 13:45:36 +0000

    point_analysis calculates point means

--- horizontal_winds/point_analysis.py
+++ horizontal_winds/point_analysis.py
@@ -3,21 +3,64 @@ import csv
 import matplotlib.pyplot as plt
 # from _future__ import division
 
-def point_analysis(i):
+def point_analysis(t0 = 7000, t1 = 9999):
     with open('Data/velocity_x.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
-        x_vel = np.zeros(2999)
+        x_vel = np.zeros(t1 - t0)
         j = -1
         for row in readCSV:
-            if j >= 7000:
-                x_vel[j-7000] = row[i]
+            if j >= t0:
+                x_vel[j-t0] = row[i]
             j += 1
 
-    plt.clf()
-    plt.plot(x_vel)
-    fname = 'plots/x_vel_point_%03d.png' % i
-    print('Saving frame', fname)
-    plt.savefig(fname)
+            plt.clf()
+            plt.plot(x_vel)
+            fname = 'plots/x_vel_point_%03d.png' % i
+            print('Saving frame', fname)
+            plt.savefig(fname)
+
+def point_statistics_x(t0 = 7000, t1 = 9999):
+    with open('Data/velocity_x.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        x_mean = np.zeros(82)
+        j = -1
+        for row in readCSV:
+            if j >= t0:
+                x_mean = x_mean + np.array(row).astype(float)
+            j += 1
+        x_mean = (1/(t1-t0))*x_mean
+        x_mean[0] = 0
+        plt.clf()
+        plt.plot(x_mean, '.')
+        fname = 'plots/x_mean_points.png'
+        print('Saving frame', fname)
+        plt.savefig(fname)
+
+def point_statistics_y(t0 = 7000, t1 = 9999):
+    with open('Data/velocity_y.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        y_mean = np.zeros(82)
+        j = -1
+        for row in readCSV:
+            if j >= t0:
+                y_mean = y_mean + np.array(row).astype(float)
+            j += 1
+        y_mean = (1/(t1-t0))*y_mean
+        y_mean[0] = 0
+        plt.clf()
+        plt.plot(y_mean, '.')
+        fname = 'plots/y_mean_points.png'
+        print('Saving frame', fname)
+        plt.savefig(fname)
+
+
+
+
+
+
+
+
+
 
 def mean_point_x():
     with open('Data/velocity_x.csv') as csvfile:
@@ -52,5 +95,4 @@ def mean_point_y():
     plt.savefig(fname)
 
 
-for i in range(20, 40):
-    point_analysis(i)
+point_statistics_y()

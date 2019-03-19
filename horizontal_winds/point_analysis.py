3565697c52d96d8845fe5973822a840075dee2ab
Thomas Gregory <tjg2118@ic.ac.uk>
2019-03-19 13:55:48 +0000

    calculating point variances in time

--- horizontal_winds/point_analysis.py
+++ horizontal_winds/point_analysis.py
@@ -23,16 +23,25 @@ def point_statistics_x(t0 = 7000, t1 = 9999):
     with open('Data/velocity_x.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         x_mean = np.zeros(82)
+        x_ssq = np.zeros(82)
         j = -1
         for row in readCSV:
             if j >= t0:
                 x_mean = x_mean + np.array(row).astype(float)
+                x_ssq = x_ssq + np.square(np.array(row).astype(float))
             j += 1
         x_mean = (1/(t1-t0))*x_mean
+        x_var = (x_ssq - (t1-t0)*np.square(x_mean))/(t1-t0)
         x_mean[0] = 0
+        x_var[0] = x_var[1]
         plt.clf()
         plt.plot(x_mean, '.')
         fname = 'plots/x_mean_points.png'
+        print('Saving frame', fname)
+        plt.savefig(fname)
+        plt.clf()
+        plt.plot(x_var, '.')
+        fname = 'plots/x_var_points.png'
         print('Saving frame', fname)
         plt.savefig(fname)
 
@@ -40,16 +49,25 @@ def point_statistics_y(t0 = 7000, t1 = 9999):
     with open('Data/velocity_y.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         y_mean = np.zeros(82)
+        y_ssq = np.zeros(82)
         j = -1
         for row in readCSV:
             if j >= t0:
                 y_mean = y_mean + np.array(row).astype(float)
+                y_ssq = y_ssq + np.square(np.array(row).astype(float))
             j += 1
         y_mean = (1/(t1-t0))*y_mean
+        y_var = (y_ssq - (t1-t0)*np.square(y_mean))/(t1-t0)
         y_mean[0] = 0
+        y_var[0] = y_var[1]
         plt.clf()
         plt.plot(y_mean, '.')
         fname = 'plots/y_mean_points.png'
+        print('Saving frame', fname)
+        plt.savefig(fname)
+        plt.clf()
+        plt.plot(y_var, '.')
+        fname = 'plots/y_var_points.png'
         print('Saving frame', fname)
         plt.savefig(fname)
 
@@ -95,4 +113,5 @@ def mean_point_y():
     plt.savefig(fname)
 
 
+point_statistics_x()
 point_statistics_y()
